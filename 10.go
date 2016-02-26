af0bd374a77dc950102e84f5493f6fcbd5d6ba52
sudan <sudan@douban.com>
2016-02-26 10:58:24 +0800

    add 10

---
+++ 10.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	max := 2000000
+	sqrt := 1414
+	var numbers = make([]int, max)
+	for i, _ := range numbers {
+		numbers[i] = i
+	}
+
+	var factor = make([]int, sqrt)
+	numbers[1] = 0
+	for i, _ := range factor[2:] {
+		if i < 2 || numbers[i] == 0 {
+			continue
+		}
+		for bad := i * 2; bad < max; bad += i {
+			numbers[bad] = 0
+		}
+	}
+
+	sum := 0
+	for _, i := range numbers {
+		sum += i
+	}
+	fmt.Println(sum)
+}
